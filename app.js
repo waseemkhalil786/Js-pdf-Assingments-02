231c384c1d6d4f6aa41c9518e9f8bd4d72e991c8
Waseem khalil <waseemkhalilworl@gmail.com>
2024-03-15 11:46:27 -0700

    2.00excercise assingment

---
+++ app.js
@@ -0,0 +1,22 @@
+// // strings variables
+
+let myName="Waseem khalil";
+
+console.log(myName);
+
+// title message alert example
+
+let message = "Hello World";
+
+alert(message);
+
+// student’s bio data in JS variables show the data in alert boxes
+
+let userName = prompt("what is your name");
+
+alert("Welcome 😊 " + userName);
+
+// . Write a script to display the following alert using one JS // variable:
+
+
+
