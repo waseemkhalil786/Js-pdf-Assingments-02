cc91338d308863529198cb6927efaeebe91d5c67
Waseem khalil <waseemkhalilworl@gmail.com>
2024-03-17 21:55:52 -0700

    Pdf 2 complete and start 4 pdf

--- app.js
+++ app.js
@@ -16,7 +16,37 @@ let userName = prompt("what is your name");
 
 alert("Welcome 😊 " + userName);
 
+console.log(userName);
+
 // . Write a script to display the following alert using one JS // variable:
 
+// alert(`pizza
+// pizz
+// piz
+// pi
+// p
+// `);
 
+alert(" Pizza \n Pizz \n Piz \n Pi \n P");  // ye 2 tariky hain jin se ham word break ya line break
 
+// 2.6
+
+let email = "waseemkhalilworld@gmail.com";
+
+alert(email);
+
+// for example
+let userEmail =prompt("Enter your email");
+
+console.log(userEmail);
+
+// 2.7
+let sentence = " A smarter way to learn JavaScript "
+
+alert(sentence);
+
+// 2.8
+
+document.write("Hello World! js practice ");
+
+//2.9 samjh ni aya
